aebc5e6fec0bc838d740eeaa50d3888aa0fcc3af
Cooper Luetje <koopaluigi@hotmail.com>
2016-12-19 14:30:58 -0500

    Test git email

--- Perspective/Request.swift
+++ Perspective/Request.swift
@@ -119,11 +119,11 @@ class Request
                 return
             }
             
-            
+            /*
              //Print out the response from the server for debugging
              let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
              print("responseString = \(responseString)")
- 
+            */
             
             var post:NSDictionary = [:]
             
